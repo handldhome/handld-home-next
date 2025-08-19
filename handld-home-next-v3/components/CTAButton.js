031103b6bbd86f198b4234883bb10ed64d594ea1
handldhome <brad@handldhome.com>
2025-08-19 11:39:23 -0700

    Delete handld-home-next-v3 directory

--- handld-home-next-v3/components/CTAButton.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function CTAButton({ children = "Get My Quote", className = "" }) {
-  return (
-    <Link href="https://handldhome.typeform.com/to/lEaYy0ka" className={`btn btn-primary no-underline ${className}`}>
-      {children}
-    </Link>
-  );
-}
